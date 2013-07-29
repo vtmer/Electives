08444cccb92dd88ee988af343056e30b51095476
sayue <likehuyi92@gmail.com>
2013-07-29 16:53:54 +0800

    add database

---
+++ Electives.sql
@@ -0,0 +1,119 @@
+-- phpMyAdmin SQL Dump
+-- version 3.5.2.2
+-- http://www.phpmyadmin.net
+--
+-- 主机: 127.0.0.1
+-- 生成日期: 2013 年 07 月 29 日 09:43
+-- 服务器版本: 5.5.27
+-- PHP 版本: 5.4.7
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- 数据库: `elective`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `collection`
+--
+
+CREATE TABLE IF NOT EXISTS `collection` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) unsigned NOT NULL,
+  `course_id` int(11) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`),
+  KEY `course_id` (`course_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `comment`
+--
+
+CREATE TABLE IF NOT EXISTS `comment` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `course_id` int(11) unsigned NOT NULL,
+  `user_id` int(11) unsigned NOT NULL,
+  `content` text COMMENT '评论内容',
+  `exam_form` varchar(20) NOT NULL COMMENT '考试形式',
+  `interest_grade` tinyint(2) NOT NULL COMMENT '趣味性',
+  `exam_grade` tinyint(2) NOT NULL COMMENT '考试难度',
+  `comment_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '评论时间',
+  PRIMARY KEY (`id`),
+  KEY `course_id` (`course_id`),
+  KEY `user_id` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `course`
+--
+
+CREATE TABLE IF NOT EXISTS `course` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL COMMENT '课程名',
+  `kind` varchar(30) NOT NULL COMMENT '课程归属',
+  `code` varchar(15) NOT NULL COMMENT '课程编号',
+  `teacher` varchar(20) NOT NULL COMMENT '任课老师',
+  `email` varchar(50) DEFAULT NULL COMMENT '联系邮箱',
+  `phone` varchar(15) DEFAULT NULL COMMENT '联系电话',
+  `intro` text NOT NULL COMMENT '简介',
+  `interest_grade` tinyint(2) NOT NULL COMMENT '趣味性',
+  `exam_grade` tinyint(2) NOT NULL COMMENT '考试难度',
+  `multiple_grade` tinyint(2) NOT NULL COMMENT '综合星级',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `code` (`code`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `user`
+--
+
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `student_id` varchar(11) NOT NULL COMMENT '学号',
+  `campus` varchar(50) NOT NULL COMMENT '校区',
+  `grade` varchar(10) NOT NULL COMMENT '年级',
+  `kickname` varchar(25) NOT NULL COMMENT '昵称',
+  `register_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '注册时间',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `student_id` (`student_id`),
+  KEY `id` (`id`),
+  KEY `id_2` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+--
+-- 限制导出的表
+--
+
+--
+-- 限制表 `collection`
+--
+ALTER TABLE `collection`
+  ADD CONSTRAINT `collection_ibfk_2` FOREIGN KEY (`course_id`) REFERENCES `course` (`id`),
+  ADD CONSTRAINT `collection_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
+
+--
+-- 限制表 `comment`
+--
+ALTER TABLE `comment`
+  ADD CONSTRAINT `comment_ibfk_1` FOREIGN KEY (`course_id`) REFERENCES `course` (`id`),
+  ADD CONSTRAINT `comment_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
